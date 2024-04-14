8ab5866616b68c18ba3fb02e6e715d6b1e2d1ff1
AdamNiesiobedzki <adam.niesiobedzki@o2.pl>
2024-04-14 17:31:24 +0200

    RSA private key pin encryption

--- RsaGenerator/src/main/java/bsk/adamniesiobedzki/rsagenerator/RsaGenerator.java
+++ RsaGenerator/src/main/java/bsk/adamniesiobedzki/rsagenerator/RsaGenerator.java
@@ -1,15 +1,26 @@
 package bsk.adamniesiobedzki.rsagenerator;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Arrays;
 import java.util.Base64;
+import java.util.Random;
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
 
 public class RsaGenerator {
+
+    private static final Random random = new SecureRandom();
+    static ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+    private static final String algorithm = "AES/CBC/PKCS5Padding";
     public static KeyPair generateRsaKeys() {
         try {
             KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
@@ -22,18 +33,72 @@ public class RsaGenerator {
         }
     }
 
-    public static void saveRsaKeys(Path path, String PIN) {
+    public static void saveRsaKeys(Path path, String pin) {
         KeyPair keyPair = generateRsaKeys();
         if (keyPair != null) {
             try (FileOutputStream publicKeyFile = new FileOutputStream(path.resolve("public_key.pem").toString());
-                 FileOutputStream privateKeyFile = new FileOutputStream(path.resolve("private_key.pem").toString())) {
+                 FileOutputStream privateKeyFile = new FileOutputStream(path.resolve("private_key.pem").toString());
+                 FileOutputStream privateKeyEncodedFile = new FileOutputStream(path.resolve("private_key_encoded.pem").toString())) {
                 byte[] publicKeyBytes = keyPair.getPublic().getEncoded();
-                publicKeyFile.write(Base64.getEncoder().encode(publicKeyBytes));
                 byte[] privateKeyBytes = keyPair.getPrivate().getEncoded();
+                SecretKey pinKey = getKeyFromPin(pin);
+                IvParameterSpec iv =  generateIv();
+                byte[] privateKeyEncryptedBytes = encrypt(privateKeyBytes, pinKey, iv);
+
                 privateKeyFile.write(Base64.getEncoder().encode(privateKeyBytes));
-            } catch (IOException e) {
+                publicKeyFile.write(Base64.getEncoder().encode(publicKeyBytes));
+                privateKeyEncodedFile.write(Base64.getEncoder().encode(privateKeyEncryptedBytes));
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
+
+    public static SecretKey getKeyFromPin(String pin)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        KeySpec spec = new PBEKeySpec(pin.toCharArray(), getNextSalt(), 65536, 256);
+        return new SecretKeySpec(factory.generateSecret(spec).getEncoded(), "AES");
+    }
+
+    public static IvParameterSpec generateIv() {
+        byte[] iv = new byte[16];
+        new SecureRandom().nextBytes(iv);
+        return new IvParameterSpec(iv);
+    }
+
+    public static byte[] getNextSalt() {
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        return salt;
+    }
+
+    public static byte[] encrypt(byte[] input, SecretKey key, IvParameterSpec iv)
+            throws NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidAlgorithmParameterException, InvalidKeyException,
+            BadPaddingException, IllegalBlockSizeException {
+        Cipher cipher = Cipher.getInstance(algorithm);
+        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+        byte[] encryptedInput = cipher.doFinal(input);
+        try {
+            outputStream.write(encryptedInput);
+            outputStream.write(iv.getIV());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return outputStream.toByteArray();
+    }
+
+    public static byte[] decrypt(byte[] input, SecretKey key)
+            throws NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidAlgorithmParameterException, InvalidKeyException,
+            BadPaddingException, IllegalBlockSizeException {
+        IvParameterSpec iv = new IvParameterSpec(Arrays.copyOfRange(input, input.length - 16, input.length));
+        byte[] encryptedBytes = Arrays.copyOfRange(input, 0, input.length - 16);
+        Cipher cipher = Cipher.getInstance(algorithm);
+        cipher.init(Cipher.DECRYPT_MODE, key, iv);
+        return cipher.doFinal(encryptedBytes);
+    }
+
 }
+
